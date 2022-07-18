345c68edfcb9a2a4efa3b8f9d310bc7a16266a15
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2022-07-18 14:25:52 +0700

    menambahkan font poppins dan menambahkan link pada social media icon

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -8,13 +8,13 @@ import './index.css';
 const SocialMedia = () => {
     return (
         <div className="social-media">
-            <a className="social-media-icon" href="#github">
+            <a className="social-media-icon" href="https://github.com/zachriek" target="_blank" rel="noreferrer">
                 <Github />
             </a>
-            <a className="social-media-icon" href="#youtube">
+            <a className="social-media-icon" href="https://www.youtube.com/channel/UCFbzQWPGA17_gKzotx207jQ" target="_blank" rel="noreferrer">
                 <Youtube />
             </a>
-            <a className="social-media-icon" href="#instagram">
+            <a className="social-media-icon" href="https://www.instagram.com/zachriek/" target="_blank" rel="noreferrer">
                 <Instagram />
             </a>
         </div>
